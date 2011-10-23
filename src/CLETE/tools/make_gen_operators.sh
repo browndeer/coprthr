5b61723e3bc1f7002d1e7eb90ccf82f2ddb04892
DAR <dar@browndeertechnology.com>
2011-10-23 19:08:01 -0400

    clete: enable-clete now default, fixed on fbsd

--- src/CLETE/tools/make_gen_operators.sh
+++ src/CLETE/tools/make_gen_operators.sh
@@ -1,2 +1,2 @@
-g++ -o gen_operators -I/disk0/richie/projects/vector/temp/src -ftemplate-depth-40 gen_operators.cpp Options.cpp PAssert.cpp PeteOps.cpp Join.cpp
+g++ -m32 -o gen_operators -I/disk0/richie/projects/vector/temp/src -ftemplate-depth-40 gen_operators.cpp Options.cpp PAssert.cpp PeteOps.cpp Join.cpp
 
